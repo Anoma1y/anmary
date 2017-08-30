da5529b865ef2e764da2039b0641654aa7a6d817
Anoma1y <ueuknjg@gmail.com>
2017-08-30 19:29:43 +0300

    y

---
+++ views/catalog/header.php
@@ -0,0 +1,39 @@
+<?php require_once '/views/index/header.php'; ?>
+<link rel="stylesheet" href="/static/css/catalog.min.css">
+<link rel="stylesheet" href="/static/css/product.min.css">
+
+        <div class="catalog_menu">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="contact">
+                            <p>+7-(963)-040-25-19</p>
+                            <p>nt-nt@mail.ru</p>
+                            <p class="time_work">Пн-вс, с 10:00 до 20:00</p>
+                        </div>                        
+                    </div>
+                    <div class="col-md-4">
+                         <div class="logo">
+                            <img src="/static/img/logo_black.png" alt="">
+                        </div>                       
+                    </div>
+                    <div class="col-md-4">
+                        <div class="adress">
+                            <p>г. Санкт Петербург, Скобелевский пр.,</p>
+                            <p>ТЦ Президентский, 2 этаж</p>
+                            <p>Остановка: метро Удельная</p>
+                        </div>                        
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <ul id="menu-catalog">
+                            <li><a href="/">Главная</a></li>
+                            <li><a href="/catalog/all">Каталог</a></li>
+                            <li><a href="/contacts">Контакты</a></li>
+                        </ul>                       
+                    </div>
+                </div>
+            </div>
+        
+        </div>
