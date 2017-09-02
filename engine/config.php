675a1af7d2388433460cb1c451121742e5e123fe
Anoma1y <ueuknjg@gmail.com>
2017-09-02 20:36:15 +0300

    s

--- engine/config.php
+++ engine/config.php
@@ -3,21 +3,28 @@
 	* Подключение моделей для контроллеров.
 	**/
 	require_once 'models/admin.php';
+
 	require_once 'models/user.php';
+
 	require_once 'models/catalog.php';
+
 	require_once 'models/product.php';
 
 	return array(
 		'home_title' => 'SITE NAME',
-		'charset' => 'utf-8',
+		'charset' => 'utf8',
 		'description' => 'Описание сайта',
 		'keywords' => 'Ключевые, Слова, Через, Запятую',
 		'site_offline' => 'yes',
 		'registration ban' => 'no',
 	    'host' => 'localhost',
-	    'dbname' => 'magazine',
-	    'user' => 'root',
+	    
+	    'dbname' => 'srv77500_anmary',
+
+	    'user' => '',
+
 	    'password' => '',
+
 	    'dbcharset' => 'utf8',
 	     //Пагинация AJAX
 	    'record_per_page' => 9, //Записей на странице
