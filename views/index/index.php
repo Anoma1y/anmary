e92f6591368f59f78ee1bc2b2d6a963eced6b810
German Voytekhovich <ueuknjg@gmail.com>
2017-12-23 22:21:32 +0300

    day 2

--- views/index/index.php
+++ views/index/index.php
@@ -24,18 +24,36 @@
 				<div class="products_info">
 					<span class="info_count">13</span>
 					<i class="fa  fa-shopping-basket" aria-hidden="true"></i>
-					<span>4500</span>					
+					<p>4500</p>					
 				</div>
 			</div>
 		</div>
 	</div>
 	<div class="menu_container">
 		<div class="header_menu">
-			<div class="container">
+			<div class="header_container">
 				<div class="search">
-					<i class="fa fa-search" aria-hidden="true"></i>
+					<i class="fa fa-search" id="searchBtn" aria-hidden="true"></i>
+					<div class="search_input">
+						<input type="text" class="searchText" id="searchText" placeholder="Поиск">
+						<button id="search"><i class="fa fa-search" aria-hidden="true"></i></button>		
+					</div>
 				</div>	
 				<nav class="navigation">
+					<div class="mobile-menu">
+						<a class="mobile-trigger" href="#" id="mobile-trigger"><span></span></a>
+						<nav class="menu-main-wrap">
+							<ul class="ui menu menu-effect_1">
+								<li><a href="#">Link-1</a></li>
+								<li><a href="#">Link-2</a></li>
+								<li><a href="#">Link-3</a></li>
+								<li><a href="#">Link-4</a></li>
+								<li><a href="#">Link-5</a></li>
+								<li><a href="#">Link-6</a></li>
+							</ul>
+						</nav>
+						<div class="menu-main-overlay"></div>
+					</div>
 					<ul>
 						<li><a href="#">Link-1</a></li>
 						<li><a href="#">Link-2</a></li>
@@ -46,7 +64,9 @@
 					</ul>
 				</nav>
 				<div class="social_network">
-					
+					<i class="fa fa-vk" aria-hidden="true"></i>
+					<i class="fa fa-skype" aria-hidden="true"></i>
+					<i class="fa fa-odnoklassniki" aria-hidden="true"></i>
 				</div>		
 			</div>
 		</div>
@@ -54,24 +74,36 @@
 </header>
 
 
-<!-- <section id="main_slider">
+<section id="main_slider">
 	<div class="container">
 		<div class="main_slider">
 			<ul class="rslides" id="slider">
 				<li>
 					<img src="static/img/slider/first.jpg" alt="Slider">
+		          <div class="caption">
+		          	<h4>SOMTHING IS BETTER</h4>
+		          	<h2>Fashion Lorrem</h2>
+		          </div>
 				</li>
 				<li>
 					<img src="static/img/slider/second.jpg" alt="Slider">
+		          <div class="caption">
+		          	<h4>SOMTHING IS BETTER</h4>
+		          	<h2>Lorem ipsum.</h2>
+		          </div>
 				</li>
 				<li>
 					<img src="static/img/slider/third.jpg" alt="Slider">
+		          <div class="caption">
+		          	<h4>SOMTHING IS BETTER</h4>
+		          	<h2>dolor sit amet.</h2>
+		          </div>
 				</li>
 			</ul>			
 		</div>
 		
 	</div>
-</section> -->
+</section>
 
 <section>
 	<div class="blocks">
@@ -134,7 +166,7 @@
 			<div class="product">
 				<div class="product__image">
 					<a href="#">
-						<img src="/static/img/product/prod-1.png" alt="Latest Product">
+						<img src="http://via.placeholder.com/238x350" alt="Latest Product">
 					</a>
 				</div>
 				<div class="product__info">
@@ -146,7 +178,7 @@
 			<div class="product">
 				<div class="product__image">
 					<a href="#">
-						<img src="/static/img/product/prod-2.png" alt="Latest Product">
+						<img src="http://via.placeholder.com/238x350" alt="Latest Product">
 					</a>
 				</div>
 				<div class="product__info">
@@ -159,7 +191,7 @@
 			<div class="product">
 				<div class="product__image">
 					<a href="#">
-						<img src="/static/img/product/prod-3.png" alt="Latest Product">
+						<img src="http://via.placeholder.com/238x350" alt="Latest Product">
 					</a>
 				</div>
 				<div class="product__info">
@@ -171,7 +203,7 @@
 			<div class="product">
 				<div class="product__image">
 					<a href="#">
-						<img src="/static/img/product/prod-4.png" alt="Latest Product">
+						<img src="http://via.placeholder.com/238x350" alt="Latest Product">
 					</a>
 				</div>
 				<div class="product__info">
@@ -183,7 +215,7 @@
 			<div class="product">
 				<div class="product__image">
 					<a href="#">
-						<img src="/static/img/product/prod-5.png" alt="Latest Product">
+						<img src="http://via.placeholder.com/238x350" alt="Latest Product">
 					</a>
 				</div>
 				<div class="product__info">
@@ -197,7 +229,55 @@
 </section>
 
 
-
+<footer>
+	<div class="container">
+		<div class="footer-left">
+			<div class="footer_logo">
+				<h1>Anmary</h1>
+			</div>
+			<div class="footer_about">
+				<h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam molestias eligendi officiis in. Voluptate officia, velit dolore. Molestias, obcaecati, quidem.</h4>
+			</div>
+			<div class="copyright">
+				<p>© 2016 - 2017 Anmary</p>
+			</div>
+		</div>
+		<div class="footer-center">
+			<nav>
+				<ul class="footer_menu">
+					<li><a href="#">Ссылка-1</a></li>
+					<li><a href="#">Ссылка-2</a></li>
+					<li><a href="#">Ссылка-3</a></li>
+					<li><a href="#">Ссылка-4</a></li>
+					<li><a href="#">Ссылка-5</a></li>
+				</ul>
+			</nav>
+			<nav>
+				<ul class="footer_menu">
+					<li><a href="#">Ссылка-1</a></li>
+					<li><a href="#">Ссылка-2</a></li>
+					<li><a href="#">Ссылка-3</a></li>
+					<li><a href="#">Ссылка-4</a></li>
+					<li><a href="#">Ссылка-5</a></li>
+				</ul>
+			</nav>
+		</div>
+		<div class="footer-right">
+			<div class="footer_contact">
+				<h3>Связаться с нами</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, maxime!</p>
+			</div>
+			<div class="footer_email">
+				<p>email@mail.ru</p>
+			</div>
+			<div class="footer_social">
+				<i class="fa fa-vk" aria-hidden="true"></i>
+				<i class="fa fa-skype" aria-hidden="true"></i>
+				<i class="fa fa-odnoklassniki" aria-hidden="true"></i>
+			</div>
+		</div>
+	</div>
+</footer>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="/static/js/responsiveslides.min.js"></script>
 <script>
@@ -205,14 +285,48 @@
 		auto: true,
 		pager: false,
 		nav: true,
-		speed: 500,
+		speed: 1000,
 		namespace: "callbacks",
-		before: function () {
+		before: function() {
 			$('.events').append("<li>before event fired.</li>");
 		},
-		after: function () {
+		after: function() {
 			$('.events').append("<li>after event fired.</li>");
 		}
 	});
+	$('#searchBtn').on('click', function() {
+		$(this).fadeOut('400');
+		$('.search_input').fadeIn('400');
+	});
+	//Mobile Menu
+	$('#mobile-trigger').bind('click', function (e) {
+		$(this).toggleClass('active');
+		e.preventDefault();
+		$('.menu-main-overlay').fadeIn('400', function () {
+			$('#menu-open').toggleClass('active');
+			$('.menu-main-wrap').toggleClass('active');
+			$('.menu-effect_1').toggleClass('active');
+		});
+	});
+
+	$('.menu-main-overlay').bind('click', function () {
+		$('.menu-main-overlay').fadeOut('400', function () {
+			$('#menu-open').removeClass('active');
+			$('#icon-toggle').removeClass('active');
+			$('.menu-main-wrap').removeClass('active');
+			$('.menu-effect_1').removeClass('active');
+		});
+	});
+	$(window).resize(function() {
+		if ($(window).width() >= 996) {
+			$('.menu-main-overlay').fadeOut('400', function () {
+				$('#menu-open').removeClass('active');
+				$('#icon-toggle').removeClass('active');
+				$('.menu-main-wrap').removeClass('active');
+				$('.menu-effect_1').removeClass('active');
+			});
+		}
+	});
+
 </script>
 <?php require_once 'views/index/footer.php'; ?>
