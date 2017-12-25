ae2aef307f6927debe89f09535295053d8a6bc40
German Voytekhovich <ueuknjg@gmail.com>
2017-12-25 18:20:16 +0300

    added pagination

--- views/catalog/index.php
+++ views/catalog/index.php
@@ -1,3 +1,4 @@
+
 <?php require_once "views/index/header.php"; ?>
 
 
@@ -10,10 +11,14 @@
 <?php foreach ($brandList as $key => $value): ?>
     <label for="brandList"><?=$value["brand_name"];?></label><input type="checkbox" class="brandList" name="brandList" value="<?=$value["id"];?>">
 <?php endforeach ?>
-
-<?php echo "<pre>"; var_dump($filter); echo "</pre>";?>
-<!-- <script src="/static/js/libs.min.js"></script> -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<div class="pagination-content">
+    <div class="pagination-button">
+        <div class="paginations"></div>
+    </div>
+</div>
+<?php //echo "<pre>"; var_dump($filter); echo "</pre>";?>
+<script src="/static/js/libs.min.js"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
 <script type="text/javascript">
     //Получение максимальной цены товара из каталога
     var maxPrice = <?php $maxPrice = array(); $i = 0; foreach ($productList as $key) { $maxPrice[$i] = $key['price']; $i++; } echo json_encode(max($maxPrice)); ?>; 
@@ -23,7 +28,7 @@
     var total_pages, 
         items, 
         currentPage = 1;
-    var page = 1;
+    // var page = 1;
     var state = {
         categoryFilter: [],
         brandFilter: [],
@@ -53,19 +58,46 @@
         }, false);
     }
 
+ load_data(currentPage, state); 
+
     //AJAX запрос для вызова каталог продуктов (принимает значения: текущая страница и объект состояний)
-    function load_data() {  
+    function load_data(page, state) {  
         $.ajax({  
             url:"getAllProduct", 
-            method:"POST",
-            data: {page: page, state: state},  
+            method:"POST",  
+            data:{page: page, state: state},  
             success:function(data){ 
                 items = $.parseJSON(data);
                 console.log(items);
+                let total_items = items['total_item'];
+                let item_on_page = items['record_per_page'];
+
+                //Установка новой текущей страницы
+                let currentPage = items['current_page'];
                 //Пагинация
-           }
+                $('.paginations').pagination({
+                    items: total_items,
+                    itemsOnPage: item_on_page,
+                    cssStyle: 'dark-theme',
+                    hrefTextPrefix: '',
+                    currentPage : currentPage,
+                    onPageClick : function(pageNumber) {
+
+                        load_data(pageNumber, state);
+                            //Изменение пагинации
+                        
+                    }
+                });                
+            }                   
+            //Переменные для создания пагинации (всего страниц, продуктов на странице, текущая страница)
+
+           
       })//AJAX 
     }
-    load_data();
+
+
+
+
+
 </script>
 <!-- <script src="/static/js/catalog.js"></script> -->
