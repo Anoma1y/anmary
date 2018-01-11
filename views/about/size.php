847e57273d3d44abfb15cfcd78f3aff99b03357a
German Voytekhovich <ueuknjg@gmail.com>
2018-01-12 02:40:09 +0300

    fixed size page

--- views/about/size.php
+++ views/about/size.php
@@ -1,35 +1,307 @@
 <?php require_once 'views/index/header.php'; ?>
+<div class="container">
+	<div class="about">
+		<div class="size-info">
+			<div class="info-content">
+				<div class="info-title">Выбор размера</div>
+				<p>Для определения подходящего размера необходимо провести измерения с помощью сантиметровой ленты и соотнести полученные измерения с размерами в таблице.</p>
 
-<table>
-	<tr>
-		<td>Российский размер</td>
-		<td>42</td>
-		<td>44</td>
-		<td>46</td>
-		<td>50</td>
-		<td>52</td>
-		<td>54</td>
-		<td>56</td>
-		<td>58</td>
-		<td>60</td>
-	</tr>
-	<tr>
-		<td>Международный</td>
+				<p>При измерении обхвата <span class="size-color size-boobs">груди</span> лента должна плотно прилегать к телу, спереди проходить по наиболее выступающим точкам, сбоку через подмышечные впадины, сзади обхватывая лопатки.</p>
 
-	</tr>
-	<tr>
-		<td>Обхват груди (см)</td>
+				<p>Обхват <span class="size-color size-waist">талии</span> измеряется строго горизонтально по самой узкой части тела, проходя через самую выступающую точку живота.</p>
 
-	</tr>
-	<tr>
-		<td>Обхват талии (см)</td>
+				<p>При измерении обхвата <span class="size-color size-hip">бедер</span> лента должна находиться горизонтально, проходя посредине бедра и сзади по наиболее выступающим точкам ягодиц.</p>				
+			</div>
+			<div class="info-image">
+				<svg viewBox="0 0 199.80489 487.80487">
+					<path class="hip" d="m 38.944794,189.60714 c -3.194822,7.0458 -3.963102,15.08825 -3.695491,18.47746 17.230273,8.4174 47.789737,11.29198 77.605317,9.9494 -1.46859,-5.28943 -4.30912,-10.0341 -3.69549,-16.77185 -25.835366,-0.71737 -50.239798,-4.24107 -70.214336,-11.65501 z"/>	
+					<path class="waist" d="m 56.908413,152.59195 c 0.08396,9.27012 -4.036088,13.44136 -5.685371,14.63984 17.048169,8.23529 21.952677,9.75988 52.305418,8.95446 -2.93683,-5.07968 -3.821238,-8.80429 0.4264,-16.06118 -25.835366,-0.71737 -27.071909,-0.11918 -47.046447,-7.53312 z"/>
+					<path class="boobs" d="m 48.907599,122.73605 c -1.42917,10.17855 2.561444,17.87716 3.553358,18.1932 20.387629,4.91151 49.187413,2.37946 60.833473,1.27921 0.29499,-2.47355 6.34141,-11.08457 6.82245,-20.04094 -22.556946,1.32125 -48.79589,6.61708 -71.209281,0.56853 z"/>
+				</svg>
+				<img src="/static/img/woman-size.png" alt="">
+			</div>
+		</div>
+		<div class="size-block">
+			<table class="female-sizes" id="female-sizes">
+				<thead>
+					<tr>
+						<th>Российский размер</th>
+						<th>Международный размер</th>
+						<th>Обхват груди (см)</th>
+						<th>Обхват талии (см)</th>
+						<th>Обхват бедер (см)</th>
+						<th>Длина рукава (см)</th>
+						<th>Англия (UK)</th>
+						<th>США (US)</th>
+						<th>Европа (EU)</th>
+						<th>Италия (IT)</th>
+						<th>Япония (JP)</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>38</td>
+						<td>XXS</td>
+						<td>76</td>
+						<td>58</td>
+						<td>82</td>
+						<td>58 - 60</td>
+						<td>4/30</td>
+						<td>0</td>
+						<td>32</td>
+						<td>36/0</td>
+						<td>3</td>
+					</tr>
+					<tr>
+						<td>40</td>
+						<td>XS</td>
+						<td>80</td>
+						<td>62</td>
+						<td>86</td>
+						<td>59 - 61</td>
+						<td>6/32</td>
+						<td>2</td>
+						<td>34</td>
+						<td>38/I</td>
+						<td>5</td>
+					</tr>
+					<tr>
+						<td>42</td>
+						<td>S</td>
+						<td>84</td>
+						<td>66</td>
+						<td>92</td>
+						<td>59 - 61</td>
+						<td>8/34</td>
+						<td>4</td>
+						<td>36</td>
+						<td>40/II</td>
+						<td>7</td>
+					</tr>
+					<tr>
+						<td>44</td>
+						<td>M</td>
+						<td>88</td>
+						<td>70</td>
+						<td>96</td>
+						<td>60 - 62</td>
+						<td>10/36</td>
+						<td>6</td>
+						<td>38</td>
+						<td>42/III</td>
+						<td>9</td>
+					</tr>
+					<tr>
+						<td>46</td>
+						<td>M</td>
+						<td>92</td>
+						<td>74</td>
+						<td>100</td>
+						<td>60 - 62</td>
+						<td>12/38</td>
+						<td>8</td>
+						<td>40</td>
+						<td>44/IV</td>
+						<td>11</td>
+					</tr>
+					<tr>
+						<td>48</td>
+						<td>L</td>
+						<td>96</td>
+						<td>78</td>
+						<td>104</td>
+						<td>60 - 62</td>
+						<td>14/40</td>
+						<td>10</td>
+						<td>42</td>
+						<td>46/V</td>
+						<td>13</td>
+					</tr>
+					<tr>
+						<td>50</td>
+						<td>L</td>
+						<td>100</td>
+						<td>82</td>
+						<td>108</td>
+						<td>61 - 63</td>
+						<td>16/42</td>
+						<td>12</td>
+						<td>44</td>
+						<td>48/VI</td>
+						<td>15</td>
+					</tr>
+					<tr>
+						<td>52</td>
+						<td>XL</td>
+						<td>104</td>
+						<td>86</td>
+						<td>112</td>
+						<td>61 - 63</td>
+						<td>18/44</td>
+						<td>14</td>
+						<td>46</td>
+						<td>50/VII</td>
+						<td>17</td>
+					</tr>
+					<tr>
+						<td>54</td>
+						<td>XXL</td>
+						<td>108</td>
+						<td>90</td>
+						<td>116</td>
+						<td>61 - 63</td>
+						<td>20/46</td>
+						<td>16</td>
+						<td>48</td>
+						<td>52/VIII</td>
+						<td>19</td>
+					</tr>
+					<tr>
+						<td>56</td>
+						<td>XXL</td>
+						<td>112</td>
+						<td>94</td>
+						<td>120</td>
+						<td>61 - 63</td>
+						<td>22/48</td>
+						<td>18</td>
+						<td>50</td>
+						<td>54/IX</td>
+						<td>21</td>
+					</tr>
+					<tr>
+						<td>58</td>
+						<td>XXXL</td>
+						<td>116</td>
+						<td>98</td>
+						<td>124</td>
+						<td>62 - 64</td>
+						<td>24/50</td>
+						<td>20</td>
+						<td>52</td>
+						<td>56/X</td>
+						<td>23</td>
+					</tr>
+					<tr>
+						<td>60</td>
+						<td>4XL</td>
+						<td>120</td>
+						<td>100</td>
+						<td>128</td>
+						<td>62 - 64</td>
+						<td>26/52</td>
+						<td>22</td>
+						<td>54</td>
+						<td>58/XI</td>
+						<td>25</td>
+					</tr>
+					<tr>
+						<td>62</td>
+						<td>4XL</td>
+						<td>124</td>
+						<td>104</td>
+						<td>132</td>
+						<td>62,5 - 65</td>
+						<td>28/54</td>
+						<td>24</td>
+						<td>56</td>
+						<td>60/XII</td>
+						<td>27</td>
+					</tr>
+					<tr>
+						<td>64</td>
+						<td>4XL</td>
+						<td>128</td>
+						<td>108</td>
+						<td>136</td>
+						<td>62,5 - 65</td>
+						<td>30/56</td>
+						<td>26</td>
+						<td>58</td>
+						<td>62/XIII</td>
+						<td>29</td>
+					</tr>
+					<tr>
+						<td>66</td>
+						<td>5XL</td>
+						<td>132</td>
+						<td>112</td>
+						<td>140</td>
+						<td>62,5 - 65</td>
+						<td>32/58</td>
+						<td>28</td>
+						<td>60</td>
+						<td>64/XIV</td>
+						<td>31</td>
+					</tr>
+					<tr>
+						<td>68</td>
+						<td>5XL</td>
+						<td>136</td>
+						<td>116</td>
+						<td>144</td>
+						<td>62,5 - 65</td>
+						<td>34/60</td>
+						<td>30</td>
+						<td>62</td>
+						<td>66/XV</td>
+						<td>33</td>
+					</tr>
+					<tr>
+						<td>70</td>
+						<td>5XL</td>
+						<td>140</td>
+						<td>120</td>
+						<td>148</td>
+						<td>62,5 - 65</td>
+						<td>36/62</td>
+						<td>32</td>
+						<td>64</td>
+						<td>68/XVI</td>
+						<td>35</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
 
-	</tr>
-	<tr>
-		<td>Обхват бедер (см)</td>
-
-	</tr>
-</table>
 
 
 <?php require_once 'views/index/footer.php'; ?>
+<script type="text/javascript">
+	var sizeHover = ['.size-boobs', '.size-hip', '.size-waist'];
+	var sizeHoverItem = ['.boobs', '.hip', '.waist'];
+	$.each(sizeHover, function(i, item) {
+		$(item).hover(
+			function() {
+				$(sizeHoverItem[i]).css({
+					'fill-opacity': '.6',
+					'stroke-opacity': '.9'
+				});
+			},
+			function() {
+				$(sizeHoverItem[i]).css({
+					'fill-opacity': '.1',
+					'stroke-opacity': '.4'
+				});
+			}
+		);		 
+	});
+	$.each(sizeHoverItem, function(i, item) {
+		$(item).hover(
+			function() {
+				$(item).css({
+					'fill-opacity': '.6',
+					'stroke-opacity': '.9'
+				});
+			},
+			function() {
+				$(item).css({
+					'fill-opacity': '.1',
+					'stroke-opacity': '.4'
+				});
+			}
+		);		 
+	});
+</script>
