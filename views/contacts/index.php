a7587616e56cc25753fe514948c58138eab50211
German Voytekhovich <ueuknjg@gmail.com>
2018-05-12 19:04:20 +0300

    edit maps

--- views/contacts/index.php
+++ views/contacts/index.php
@@ -65,6 +65,6 @@
 <?php require_once 'views/index/footer.php'; ?>
 <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyADny6Mjy49YDCexGxeRlvNFbi0QdbVqRA&callback=initMap"></script>
 <script>
-	function initMap(){var a={lat:lat,lng:lng},n=new google.maps.Map(document.getElementById("map"),{zoom:17,center:a});new google.maps.Marker({position:a,map:n})}var lat=59.927387,lng=30.345936;
+	function initMap(){var a={lat:lat,lng:lng},n=new google.maps.Map(document.getElementById("map"),{zoom:17,center:a});new google.maps.Marker({position:a,map:n})}var lat=60.0067346,lng=30.2571044;
 </script>
 <script type="text/javascript" src="/static/js/contacts.js"></script>
